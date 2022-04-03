e8816e6d2b968a6405e6313e3acf9e2d2488842a
Kong <duguyihou@gmail.com>
2022-04-03 19:09:45 +1000

    feat: use react three fiber (#8)
    Co-authored-by: kong <kong.hou@nine.com.au>

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,5 +1,28 @@
+import { Canvas, MeshProps } from '@react-three/fiber'
+import { useRef } from 'react'
+import * as THREE from 'three'
+
+function Box(props: MeshProps) {
+  const { position } = props
+  const mesh = useRef<THREE.Mesh>(null!)
+  return (
+    <mesh position={position} ref={mesh}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial />
+    </mesh>
+  )
+}
 function Home() {
-  return <div>Home</div>
+  return (
+    <div>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </div>
+  )
 }
 
 export default Home
