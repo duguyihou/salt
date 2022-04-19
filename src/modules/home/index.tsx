2b8ee16ec683c1e133dc11edaa3b0d8548f91206
kong <duguyihou@gmail.com>
2022-04-20 08:09:19 +1000

    feat: use horde

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,26 +1,9 @@
-import { Canvas, MeshProps } from '@react-three/fiber'
-import { useRef } from 'react'
-import * as THREE from 'three'
+import Scene from './Scene'
 
-function Box(props: MeshProps) {
-  const { position } = props
-  const mesh = useRef<THREE.Mesh>(null!)
-  return (
-    <mesh position={position} ref={mesh}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial />
-    </mesh>
-  )
-}
 function Home() {
   return (
     <div>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
+      <Scene />
     </div>
   )
 }
